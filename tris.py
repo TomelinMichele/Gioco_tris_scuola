81386ca0c5ca8815ff9b01c8b22304140379c808
TomelinMichele <45099795+TomelinMichele@users.noreply.github.com>
2019-12-08 13:07:27 +0100

    Update
    uso di enum

--- tris.py
+++ tris.py
@@ -1,88 +1,87 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import ftrobopy
+from enum import Enum
 import time
 
-# Connessione al controller
-txt = ftrobopy.ftrobopy('auto')
-
-#Dati....
+# Dati....
 
 __author__= "Tomelin Michele,Luca Manini"
 __email__ = "tomelinmichele@gmail.com"
 __version__= "1.0"
-__status__= "Sviluppamento"
+__status__= "Beta"
 __date__= "23/11/2019"
 
 # Dove sono collegate le varie componenti??
-    #Controller dx= master = M
-    #Controller sx= master = E
-    
-#Definizione componenti
-    # mot= motore
-    # lamp= lampadina
-    # btn= bottone/tasto    
-    # fot = fotocellula
-    # cal = Calamita
+# Controller dx= master = M
+# Controller sx= master = E
 
-mot_alz_dx = 1 #motore dx M
-mot_alz_sx = 2 #motore sx M
-mot_int = 3 # M
-mot_est = 4 # M
+# Definizione componenti
+# mot= motore
+# lamp= lampadina
+# btn= bottone/tasto
+# fot = fotocellula
+# cal = Calamita
 
-lamp_rossa = 5 # E
-lamp_verde = 6 # E
-lamp_pareggio = 3 #arancio E
-lamp_vince_robot = 1 #bianca sx E
-lamp_vince_umano = 2 #bianca dx E
+class MotorsCodes(Enum):
+    mot_alz_dx = 1          # motore dx M
+    mot_alz_sx = 2          # motore sx M
+    mot_int = 3             # M
+    mot_est = 4             # M
 
-btn_vince_robot = 7 # M
-btn_vince_umano = 8 # M
-btn_fine_corsa_alzata = 1 #in basso M
-btn_fine_corsa_rot_int_dx = 2 # M
-btn_fine_corsa_rot_int_sx = 3 # M
-btn_fine_corsa_rot_est_dx = 5 # M
-btn_fine_corsa_rot_est_sx = 4 # M
+MOTORS = None      # will be set by init
 
-fot_11 = 6 # M
-fot_12 = 7 # M
-fot_13 = 8 # m
-fot_21 = 1 # E
-fot_22 = 2 # E
-fot_23 = 3 # E
-fot_31 = 4 # E
-fot_32 = 5 # E
-fot_33 = 6 # E
+class LampsCodes(Enum):
+    lamp_rossa = 5          # E
+    lamp_verde = 6          # E
+    lamp_pareggio = 3       # arancio E
+    lamp_vince_robot = 1    # bianca sx E
+    lamp_vince_umano = 2    # bianca dx E
 
-calamita = 6 # E
+LAMPS = None       # will be set by init
 
-# Dichiarazione
+class ButtonsCodes(Enum):
+    btn_vince_robot = 7       # M
+    btn_vince_umano = 8       # M
+    btn_fine_corsa_alzata = 1 # in basso M
+    btn_fine_corsa_rot_int_dx = 2 # M
+    btn_fine_corsa_rot_int_sx = 3 # M
+    btn_fine_corsa_rot_est_dx = 5 # M
+    btn_fine_corsa_rot_est_sx = 4 # M
 
-ALL_MOTORS = INT, EXT, ALZ_DX, ALZ_SX = range(4)
-motors = [txt.motor(mot) for mot in ALL_MOTORS]
+BUTTONS = None     # will be set by init
 
-ALL_BUTTONS = ALZATA, INT_DX, INT_SX, EXT_DX, EXT_SX = range(5)
-buttons = [txt.input(but) for but in ALL_BUTTONS]
+class PhotosCodes(Enum):
+    photo_11 = 6 # M
+    photo_12 = 7 # M
+    photo_13 = 8 # m
+    photo_21 = 1 # E
+    photo_22 = 2 # E
+    photo_23 = 3 # E
+    photo_31 = 4 # E
+    photo_32 = 5 # E
+    photo_33 = 6 # E
 
-ALL_LAMP_COLORS = ROSSA, VERDE, ARANCIO, BIANCA_1, BIANCA_2 = range(5)
-lampadine = [txt.ouput(lamp) for lamp in ALL_LAMP_COLORS]
+PHOTOCELLS = None  # will be set by init
 
-fotocellule = {
-    (1,1): txt.input(fot_11),
-    (1,2): txt.input(fot_12),
-    (1,3): txt.input(fot_13),
-    (2,1): txt.input(fot_21),
-    (2,2): txt.input(fot_22),
-    (2,3): txt.input(fot_23),
-    (3,1): txt.input(fot_31),
-    (3,2): txt.input(fot_32),
-    (3,3): txt.input(fot_33),
+pc_codes_by_pos = {
+    (1,1): PhotosCodes.photo_11,
+    (1,2): PhotosCodes.photo_12,
+    (1,3): PhotosCodes.photo_13,
+    (2,1): PhotosCodes.photo_21,
+    (2,2): PhotosCodes.photo_22,
+    (2,3): PhotosCodes.photo_23,
+    (3,1): PhotosCodes.photo_31,
+    (3,2): PhotosCodes.photo_32,
+    (3,3): PhotosCodes.photo_33,
 }
 
-calamita = txt.output(calamita)
+def get_pc_by_pos(r,c):
+    k = pc_codes_by_pos[(r,c)]
+    return photocells[k]
 
-#Variabili booleane
+calamita_code = 6 # E
+CALAMITA = None   # will be set by init
 
 robot_ha_vinto = False
 umano_ha_vinto = False
@@ -95,17 +94,19 @@ init_finita = False
 nuova_partita = False
 partita_finita = False
 
-ALL_STATES = FREE, FULL = 0, 1
+class GridStates(Enum):
+    free = 1
+    full = 2
 
-grid_state = {(r,c): FREE
+cells_state = {(r,c): GridStates.free
               for r in range(3)
               for c in range(3)}
 
 def is_pos_free(r, c):
-    return grid_state[(r,c)] ==  FREE 
+    return grid_state[(r,c)] ==  GridStates.free
 
 def set_pos_state(r, c, state):
-    assert state in ALL_STATES
+    assert state in GridStates
     grid_state[(r,c)] = state
 
 # Tempo di attesa finito il quale la mossa passa al giocatore
@@ -115,8 +116,10 @@ def set_pos_state(r, c, state):
 turno_giocatore = 50
 
 #Lampadina e motori ON_OFF
-on = 512
-off = 0
+SPEED_ON = 512
+SPEED_OFF = 0
+LEVEL_ON = 512
+LEVEL_OFF = 0
 
 #Coordinate: posizioni scacchiera e scivolo le coordinate sono 3 :
   #impulsi rot1 = interna
@@ -135,70 +138,63 @@ movement_dic = {
     (3,2): [1,2,3],
     (3,3): [1,2,3],
     SCI_POS: [5,5,5], # scivolo
-} 
+}
 
 def get_movement(r, c):
     return movement_dic[(r, c)]
 
-#Implementazione funzioni
-
 def accendi_lampadina(lamp):
-    lamp.setLevel(on)
-    
+    lamp.setLevel(LEVEL_ON)
+
 def spegni_lampadina(lamp):
-    lamp.setLevel(off)
-    
+    lamp.setLevel(LEVEL_OFF)
+
 def lampeggio_lampadina(lamp, lamp_max, sleep_time=0.5):
-    # Questa funzione passatogli un numero di lampeggi massimo e la
-    # lampadina fa lampeggiare quella lampadina n volte dove è il
-    # numero di lampeggi massimo
+    # Questa funzione fa lampeggiare LAMP per LAMP_MAX volte ad
+    # intervalli di SLEEP_TIME secondi.
     for _ in range(lamp_max):
         accendi_lampadina(lamp)
         time.sleep(sleep_time)
         spegni_lampadina(lamp)
         time.sleep(sleep_time)
-        
-def mot_Off(m):
+
+def mot_off(m):
    m.stop()
-    
-def mot_On(m):
-   m.setSpeed(on) 
+
+def mot_on(m):
+   m.setSpeed(SPEED_ON)
 
 def muovi_to_row_col(r, c):
     i, e, a = get_movement(r,c)
     muovi_rot_to_pos(i, e, a)
-    
+
 def muovi_rot_to_pos(i, e, a):
     muovi_rot_int(i)
     muovi_rot_est(e)
     muovi_alzata(a)
-    
+
 def muovi_rot_int(i):
     # MUOVO ROT INTERNA TODO
     pass
-    
+
 def muovi_rot_est(i):
     # MUOVO ROT ESTERNA TODO
     pass
-    
+
 def muovi_alzata(i):
     # MUOVO ALZATA TODO
     pass
-    
-#----------------------------------------------------------------------------------------             
-    
+
 def attiva_calamita():
     calamita.setLevel(on)
 
 def spegni_calamita():
     calamita.setLevel(off)
-    
-#----------------------------------------------------------------------------------------             
-    
+
 def prendi_pallina_dallo_scivolo():
     muovi_to_row_col(SCI_POS)
     attiva_calamita()
-    
+
 def posiziona_pallina(r, c):
     prendi_pallina_dallo_scivolo()
     muovi_to_row_col(r, c)
@@ -209,21 +205,40 @@ def init():
 
     # emette un suono per segnalare l'inizio di questa fase
     # TODO
-    
+
+    import ftrobopy
+    txt = ftrobopy.ftrobopy('auto')
+
+    global MOTORS
+    MOTORS = {e:txt.motor(e.value) for e in MotorsCodes}
+
+    global LAMP
+    LAMP = {e:txt.ouput(e.value) for e in LampsCodes}
+
+    global BUTTONS
+    BUTTONS = {e:txt.input(e.value) for e in ButtonsCodes}
+
+    global PHOTOCELLS
+    PHOTOCELLS = {e:txt.input(e.value) for e in PhotosCodes}
+
+    global CALAMITA
+    CALAMITA = txt.output(calamita_code)
+
     # all'inizio i motori sono spenti
-    for o in (motore_interno, motore_esterno, motore_alzata_dx, motore_alzata_sx):
+    for o in MOTORS:
         mot_off(o)
-    
-    # comprende il posizionamento del braccio
-    # todo
-    
+
+    # comprende il posizionamento del braccio ???
+    # TODO
+
     # accensione e spegnimento lampadina per tot secondi
-    for o in (lamp_rossa, lamp_verde, lamp_arancio, lamp_bianca1, lamp_bianca2):
+    for o in LAMPS:
         lampeggio_lampadina(o, 10)
-    
-    # finita questa fase il programma attende che l'utente inizi una nuova partita...
+
+    # finita questa fase il programma attende che l'utente inizi una
+    # nuova partita...
     init_finita = true
-    
+
 def richiesta_nuova_partita():
     #TODO
     pass
@@ -231,4 +246,3 @@ def richiesta_nuova_partita():
 def strategia():
     #TODO
     pass
-    
