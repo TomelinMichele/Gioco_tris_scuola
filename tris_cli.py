afe4720eaa32c621429984a7578edb3ca0cff70c
prof. manini <luca.manini@scuole.provincia.tn.it>
2019-12-11 16:25:36 +0100

    Add position stuff.

--- tris_cli.py
+++ tris_cli.py
@@ -11,6 +11,10 @@ except ImportError:
 hist_file = os.path.expanduser('~/.tris_history')
 hist_file_size = 1000
 
+pos_file = "motor_pos.txt"
+cur_pos = [0,0,0]
+all_pos = dict()
+
 class Cli(cmd.Cmd):
 
     def __init__(self, boss=None):
@@ -19,6 +23,12 @@ class Cli(cmd.Cmd):
         self.prompt = "tris> "
         self.boss = boss
 
+    def do_save_pos(self, line):
+        with open(pos_file, "w") as f:
+            for k,v in all_pos.items():
+                v = " ".join(map(str, v))
+                f.write(f"{k} {v}\n")
+        
     def preloop(self):
         if readline and os.path.exists(hist_file):
             readline.read_history_file(hist_file)
@@ -50,36 +60,56 @@ class Cli(cmd.Cmd):
     def do_alza(self, line):
         "Alza il motore"
         y = int(line.split()[0])
-        tris.alza(y)
+        cur_pos[0] += y
+        # tris.alza(y)
 
     def do_abbassa(self, line):
         "Abbassa il motore"
         y = int(line.split()[0])
-        tris.abbassa(y)
+        cur_pos[0] -= y        
+        # tris.abbassa(y)
 
-    def do_gira_dx(self, line):
+    def do_ruota_interno(self, line):
         "Gira a destra il motore"
         y = int(line.split()[0])
-        tris.gira_a_dx(y)
+        cur_pos[1] += y        
+        # tris.ruota_interno(y)
 
-    def do_gira_sx(self, line):
+    def do_ruota_esterno(self, line):
         "Gira a sinistra il motore"
         y = int(line.split()[0])
-        tris.gira_a_dx(y)
+        cur_pos[2] += y        
+        # tris.ruota_esterno(y)
 
     def do_memorizza_pos(self, line):
         "Memorizza la posizione"
-        y = int(line.split()[0])
-        f.write(str(y))
-        print("Posizione salvata")
+        global cur_pos
+        motor = line.split()[0]
+        # tris.memorizza_pos(motor)
+        print(f"Posizione {motor} {cur_pos} salvata")
+        all_pos[motor] = cur_pos
+        cur_pos = [0,0,0]
 
-
+    def complete_lamp(self, text, line, begidx, endidx):
+        ss = "lamp_rossa lamp_verde lamp_pareggio lamp_vince_robot lamp_vince_umano".split()
+        oo = line.split()
+        text = oo[-1]
+        if len(oo) == 2 and line[-1].strip():
+            ww = "lamp_rossa lamp_verde lamp_pareggio lamp_vince_robot lamp_vince_umano"
+        elif len(oo) == 3:      # TODO FIX THIS
+            ww = "on off"
+        ss = ww.split()
+        if not text:
+            return ss.copy()
+        cc = [s for s in ss if s.startswith(text)]
+        return cc
+        
+    def do_lamp(self, line):
+        lamp, state = line.split()
+        print(f"tris.lamp({lamp}, {state})")
+        
 if __name__ == "__main__":
 
     c = Cli()
     c.run()
 
-# Local Variables:
-# tab-always-indent: nil
-# tab-width: 4
-# End:
