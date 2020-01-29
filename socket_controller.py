2e7fecfe78f48be3ca2d9a986768e11c334e5499
TomelinMichele <45099795+TomelinMichele@users.noreply.github.com>
2020-01-29 17:08:45 +0100

    Update socket_controller.py

--- socket_controller.py
+++ socket_controller.py
@@ -5,8 +5,6 @@
 from enum import Enum
 import time
 
-
-
 __author__= "Tomelin Michele,Luca Manini"
 
 __email__ = "tomelinmichele@gmail.com"
@@ -17,6 +15,11 @@ __status__= "Beta"
 
 __date__= "23/11/2019"
 
-ip="192.168.7.3"
+ip="192.168.7.2"
 ip2="192.168.7.4"
 ip3="192.168.7.5"
+idx="3"
+
+ import ftrobopy
+ txt = ftrobopy.ftrobopy(host=ip,port=65000) 
+ txt.play_sound(idx,1,100)
