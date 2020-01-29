bf196836f01796d18073d0433c23c0e0640f2552
TomelinMichele <45099795+TomelinMichele@users.noreply.github.com>
2020-01-29 16:59:22 +0100

    Update socket_controller.py

--- socket_controller.py
+++ socket_controller.py
@@ -1,1 +1,22 @@
 
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from enum import Enum
+import time
+
+
+
+__author__= "Tomelin Michele,Luca Manini"
+
+__email__ = "tomelinmichele@gmail.com"
+
+__version__= "1.0"
+
+__status__= "Beta"
+
+__date__= "23/11/2019"
+
+ip="192.168.7.3"
+ip2="192.168.7.4"
+ip3="192.168.7.5"
